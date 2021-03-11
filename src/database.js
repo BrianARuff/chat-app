3eceabeece13ad75fa8a24e2de1220d1e6082976
BrianARuff <briananthonyruff@gmail.com>
2021-03-11 17:44:54 -0500

    updated to code to handle production database

--- src/database.js
+++ src/database.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const database = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.MYSQL_PASSWORD,
-  database: "CHAT_APP",
-});
+let database;
+
+if (process.env.NODE_ENV === "production") {
+  database = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+  database = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "CHAT_APP",
+  });
+}
 
 database.connect();
 
