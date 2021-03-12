1022ed4a2a1ed0df5cdfe804d7cfbc182ebb1a2a
BrianARuff <briananthonyruff@gmail.com>
2021-03-12 00:51:26 -0500

    added socket io to client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
 import UserList from "../components/UserList";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
-// @ts-ignore
-const Home = (): JSX.Element[] => <UserList />;
+const Home = () => {
+  const [hello, setHello] = useState({ hello: "" });
+  useEffect(() => {
+    const socket = io.io("http://localhost:8080/");
+    socket.on("now", data => {
+      setHello(data.message);
+    });
+    console.log(socket);
+  }, []);
+  return (
+    // @ts-ignore
+    <UserList />
+  );
+};
 
 export default Home;
